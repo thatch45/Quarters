1297538ac5d9a6e9e1c3116c03c267eee7042016
Thomas S Hatch <thatch45@gmail.com>
2011-01-29 15:26:24 -0700

    Added some nothing to https.py

---
+++ quarters/https.py
@@ -0,0 +1,18 @@
+'''
+The quarters.https module provides two main things, the https server that is
+used to post communication information with the builder, and the communication
+calls to retrive the json from the master and builders.
+'''
+import urllib.request
+import zlib
+import json
+import random
+import yaml
+
+def get_master_file():
+    '''
+    '''
+
+def get_build_pkgs():
+    '''
+    '''

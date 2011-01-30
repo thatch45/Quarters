3151c8646d4fabc6fa416b8cf0f95b4121170c50
Thomas Dziedzic <gostrc@gmail.com>
2011-01-30 02:00:01 -0600

    add warning to download()

--- quarters/utils.py
+++ quarters/utils.py
@@ -3,6 +3,7 @@ import urllib.request
 def download( url ):
     filename = url.split('/')[-1]
     try:
+        # FIXME: the following function doesn't actually check if an ssl cert is good
         urllib.request.urlretrieve( url, filename )
     except ContentTooShortError as e:
         print( 'failed to retrieve %s' % url )

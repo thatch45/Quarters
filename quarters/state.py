84e9d965949c88a88248b315f8656c1889aa697c
Thomas Dziedzic <gostrc@gmail.com>
2011-01-30 12:45:25 -0600

    add state, hermes: state fetcher might remove in future, and some more job manager expansion

---
+++ quarters/state.py
@@ -0,0 +1,57 @@
+import urllib.request
+
+class State:
+    '''
+
+    base class for states
+
+    '''
+
+    def __init__( self, url, state={} ):
+        self.url = url
+        self.state = state
+
+    def fetch_state( self ):
+        # download & return json state file here
+        # FIXME: urlopen doesn't do https verification
+        json_state = urllib.request.urlopen( url ).read()
+        return json_state
+
+    def update( self ):
+        pass
+
+class MasterState( State ):
+    '''
+
+    master state data structure
+
+    '''
+
+    def update( self ):
+        json_state = fetch_state()
+
+        # do hardcore processing here
+
+class BuilderState( State ):
+    '''
+
+    builder state data structure
+
+    '''
+
+    def update( self ):
+        json_state = fetch_state()
+
+        # do hardcore processing here
+
+class BuilderAssignmentState( State ):
+    '''
+
+    builder assignment state structure
+
+    '''
+
+    def update( self ):
+        json_state = fetch_state()
+
+        # do hardcore processing here

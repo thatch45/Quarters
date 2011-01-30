f4e1ef57d5dcaf46a083d08473b86db4fdb4174a
Thomas Dziedzic <gostrc@gmail.com>
2011-01-30 15:51:16 -0600

    initial implementation of State

--- quarters/state.py
+++ quarters/state.py
@@ -1,23 +1,39 @@
 import urllib.request
+import threading
+import json
 
 class State:
     '''
 
     base class for states
 
+    implementation: one reader or one writer at a time,
+                    could make it multiple readers or one writer
+                    at a time in future, might be unnecessary
+
     '''
 
     def __init__( self, url, state={} ):
         self.url = url
-        self.state = state
+        self._state = state
+        self._statelock = threading.Lock()
 
-    def fetch_state( self ):
-        # download & return json state file here
-        # FIXME: urlopen doesn't do https verification
-        json_state = urllib.request.urlopen( url ).read()
-        return json_state
+    def get( self ):
+        self._statelock.acquire()
+        temp_state  = self._state
+        self._statelock.releaes()
+
+        return temp_state
+
+    def set( self, state ):
+        self._statelock.acquire()
+        self._state = state
+        self._statelock.releaes()
 
     def update( self ):
-        json_state = fetch_state()
+        # FIXME: urlopen doesn't do https verification
+        json_state = urllib.request.urlopen( url ).read()
 
-        # do hardcore processing here
+        state = json.loads( json_state )
+
+        set_state( self, json_state )

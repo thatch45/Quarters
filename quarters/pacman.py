3c118001951d2cad437877cbd25368f3f1cd3f84
Thomas S Hatch <thatch45@gmail.com>
2011-02-13 21:58:01 -0700

    the pacman module should work now

--- quarters/pacman.py
+++ quarters/pacman.py
@@ -40,50 +40,69 @@ class Pacman:
         '''
         self.opts = opts
         self.last_sync = 0
-        self.__setup_env()
+        self.roots = self.__setup_env()
+        self.repos = self.__grep_repos()
+        self.sync_pacman()
 
     def __setup_env(self):
         '''
         Prepare the 2 arch's pacman environments for executing pacman queries.
         '''
-        pass
+        roots = {'i686': os.path.join(self.opts['pacman_root'], 'i686'),
+                 'x86_64': os.path.join(self.opts['pacman_root'], 'x86_64')]
+        for root in roots:
+            p_dir = os.path.join(roots[root], 'var/lib/pacman')
+            if not os.path.exists(roots[root]):
+                os.makedirs(roots[root])
+            if not os.path.exists(p_dir):
+                os.makedirs(p_dir)
+            # This needs fixing, so that there is a single pacman.conf that 
+            # controls this stuff but is read like a template
+            shutil.copy(os.path.join('/etc/quarters/pacman.', root + '.conf' ),
+                    os.path.join(roots[root], 'pacman.conf')
+        return roots
+
+    def __grep_repos(self):
+        '''
+        returns the repos that are active in the pacman configurations used
+        for the repos
+        '''
+        repos = {}
+        for root in self.roots:
+            repos[root] = []
+            pac_conf = os.path.join(self.roots[root], 'pacman.conf')
+            lines = open(pac_conf, 'r').readlines()
+            for line in lines:
+                if line.startswith('#'):
+                    continue
+                if line.startswith('[options]'):
+                    continue
+                if line.startswith('['):
+                    repos[root].append(line.split('[')[1].split(']')[0])
+        return repos
 
     def sync_pacman(self):
         '''
-        Just run pacman -Sy
+        Just run pacman -Sy for the pacman environments
         '''
-        cmd = 'pacman -Sy'
-        subprocess.getoutput(cmd, shell=True)
+        for root in self.roots:
+            cmd = 'pacman --root ' + root + ' --config '\
+                + os.path.join(self.roots[root], 'pacman.conf') + ' -Sy'
+            subprocess.getoutput(cmd, shell=True)
 
-    def check_packages(self, names, repos):
+    def repo_pkgs(self):
         '''
-        Check to see if the packages are available in a repo. Returns the packages
-        that are not available
-        Arguments - names - set()
-                    repos - []
-        returns - names - set()
+        Returns a dict of dictonaries of packages:
+        {'<package name>': {'version': '<version>',
+                            'repo': '<repo>'}
         '''
-        cmd = 'pacman -Sl ' + ' '.join(repos)
-        out = subprocess.getoutput(cmd)
-        if out.startswith('error:'):
-            return names
-        pkgs = set()
-        for line in out.split('\n'):
-            pkgs.add(line.split()[1])
-        return names.difference(pkgs)
-
-    def get_pacman_pkgs(self, repos=['core', 'community', 'extra', 'multilib']):
-        '''
-        Returns a dict of {'pkg_name': 'version'} of all available packages from
-        pacman.
-        '''
-        cmd = 'pacman -Sl ' + repos
-        out = subprocess.getoutput(cmd)
-        if out.startswith('error:'):
-            return names
         pkgs = {}
-        for line in out.split('\n'):
-            comps = line.split
-            pkgs[comps[1]] = comps[2]
+        for root in self.roots:
+            pkgs[root] = {}
+            cmd = 'pacman --root ' + root + ' --config '\
+                + os.path.join(self.roots[root], 'pacman.conf')\
+                + ' -Sl ' + ' '.join(self.repos[root])
+            for line in subprocess.getoutput(cmd).splitlines():
+                comps = bytes.decode(line).split()
+                pkgs[root][comps[1]] = {'repo': comps[0], 'version': comps[2]}
         return pkgs
-

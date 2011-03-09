7c57ae3be0d8f69e2669eca4faccbb6b1bba87a8
Thomas Dziedzic <gostrc@gmail.com>
2011-03-09 06:46:53 +0000

    move job adding to joboverlord.run

--- quarters/builder/jobmanager.py
+++ quarters/builder/jobmanager.py
@@ -5,6 +5,7 @@ import os
 import tarfile
 from multiprocessing import Process, Queue
 import subprocess
+import time
 
 class JobOverlord( threading.Thread ):
     ''' controls all the poor joblings running on the server '''
@@ -18,11 +19,28 @@ class JobOverlord( threading.Thread ):
         self.chroot_base = config['chroot_base']
 
     def run( self ):
+        # start all the workers
         for worker_id in range( self.max_jobs ):
             p = Process( target=worker, args=( self.pending_jobs, worker_id, self.job_states, self.chroot_base ) )
             p.start()
             self.processlist.append( p )
 
+        # add packages (FAKE it for now since we don't have a working server to test it on)
+        i = 0
+        while 1:
+            # keep 1 job in the buffer
+            if self.pending_jobs.qsize() < 1:
+                pkgname = 'libuser'
+                pkgurl = 'https://aur.archlinux.org/packages/' + pkgname + '/' + pkgname + '.tar.gz'
+
+                # job description: ujid, pkgname, pkgsrc
+                jd = JobDescription( i, pkgname, pkgurl )
+                self.add_job( jd )
+
+                time.sleep( 2 )
+                i += 1
+
+        # join all the processes, could probably remove this
         for p in self.processlist:
             p.join()
 
@@ -39,7 +57,7 @@ def worker( job_queue, worker_id, job_states, chroot_base ):
         job_states[ current_job.ujid ] = 'inprogress'
 
         ###### start building
-        job_path = os.path.join( '/var/tmp/quarters/', current_job.ujid )
+        job_path = os.path.join( '/var/tmp/quarters/', str(current_job.ujid) )
         pkgsrc_path = os.path.join( job_path, current_job.package_name + '.tar.gz' )
         pkg_path = os.path.join( job_path, current_job.package_name )
         chroot_path = os.path.join( chroot_base, str( worker_id ) )

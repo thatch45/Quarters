d9fc66adc7c373598ff5de84812c2b4a248e17ca
Thomas Dziedzic <gostrc@gmail.com>
2011-03-23 18:16:03 -0500

    have a more consistent style in protocol.py

--- quarters/protocol.py
+++ quarters/protocol.py
@@ -26,16 +26,13 @@ def master_state( config ):
     Download the master state file and translate the data to a structure
     '''
     ret = {}
-
     url = foreign_url( config[ 'master' ], config[ 'master_port' ] ) + '/global_status'
-
     try:
         json_data = get_url( url )
         status = json.loads( json_data.decode('utf-8' ) )
         ret.update( status )
     except:
         print( 'failed to retrieve master state' )
-
     return ret
 
 def builder_states( config ):
@@ -43,27 +40,26 @@ def builder_states( config ):
     Retrieve the state of the builders
     '''
     ret = {}
-
     for ip in config[ 'builders' ]:
         url = foreign_url( ip, config[ 'builder_port' ] ) + '/global_status'
-
         try:
             json_data = get_url( url )
         except:
            continue
-
         status = json.loads( json_data.decode('utf-8' ) )
-
         ret.update( { ip : status } )
-
     return ret
 
 def get_url( url ):
-    ''' returns the contents at the url '''
+    '''
+    returns the contents at the url
+    '''
     return urllib.request.urlopen( url ).read()
 
 def get_package_list( ip, ujid, config ):
-    ''' gets a dictionary representing the object at http://ip:port/ujid/list_of_packages '''
+    '''
+    gets a dictionary representing the object at http://ip:port/ujid/list_of_packages
+    '''
     port = int( config[ 'builder_port' ] )
     url = foreign_url( ip, port ) + '/' + ujid + '/list_of_packages'
     return json.loads( get_url( url ).decode( 'utf-8' ) )
@@ -86,6 +82,9 @@ def get_packages( ip, ujid, config ):
         urllib.request.urlretrieve( url_to_dl, pkg_path )
     
 def get_build_log( ip, ujid, config ):
+    '''
+    gets the build log from the ip and ujid
+    '''
     port = int( config[ 'builder_port' ] )
     baseurl = 'http://' + ip + ':' + str(port) + '/' + ujid 
     root_ujid_path = os.path.join( config[ 'master_root' ], str(ujid) )

9b72a50f769c01a12bf801759f24f71ac61ab619
Thomas Dziedzic <gostrc@gmail.com>
2011-03-22 23:37:32 -0500

    get rid of unnecessary variable in AURRSS.get_jobs

--- quarters/scm/aurrss.py
+++ quarters/scm/aurrss.py
@@ -10,23 +10,25 @@ class AURRSS:
 
     def get_jobs( self ):
         ''' returns a list of new jobdescriptions '''
-        temp = urllib.request.urlopen( 'http://aur.archlinux.org/rss.php' ).read().decode( 'ascii' ).split('\n')[16].split('>')[1].split('<')[0]
+        cur_pkgname = urllib.request.urlopen( 'http://aur.archlinux.org/rss.php' ).read().decode( 'ascii' ).split('\n')[16].split('>')[1].split('<')[0]
 
-        if self.prev_pkgname == temp:
+        if self.prev_pkgname == cur_pkgname:
             return []
 
-        self.prev_pkgname = temp
-        pkgname = temp
         new_ujid = str( uuid.uuid4() )
-        pkgurl = 'http://' + self.master + ':' + str( self.master_port ) + '/' + str( new_ujid ) + '/' + pkgname + '.tar.gz'
+        pkgurl = 'http://' + self.master + ':' + str( self.master_port ) + '/' + str( new_ujid ) + '/' + cur_pkgname + '.tar.gz'
 
         job_path = os.path.join( self.master_root, str( new_ujid ) )
-        pkgsrc_path = os.path.join( job_path, pkgname + '.tar.gz' )
-        remote_url = 'https://aur.archlinux.org/packages/' + pkgname + '/' + pkgname + '.tar.gz'
+        pkgsrc_path = os.path.join( job_path, cur_pkgname + '.tar.gz' )
+        remote_url = 'https://aur.archlinux.org/packages/' + cur_pkgname + '/' + cur_pkgname + '.tar.gz'
         os.makedirs( job_path, exist_ok=True )
         urllib.request.urlretrieve( remote_url, pkgsrc_path )
 
-        # job description: ujid, pkgname, pkgsrc, sha256sum of srcpkg, architecture to build (x86_64,i686,any)
-        jd = JobDescription( str( new_ujid ), pkgname, pkgurl, 'sha256sumgoeshere', 'x86_64' )
+        # TODO verify sha256sum here
+
+        # job description: ujid, cur_pkgname, pkgsrc, sha256sum of srcpkg, architecture to build (x86_64,i686,any)
+        jd = JobDescription( str( new_ujid ), cur_pkgname, pkgurl, 'sha256sumgoeshere', 'x86_64' )
+
+        self.prev_pkgname = cur_pkgname
 
         return [ jd ]

a283ef2905b970f343c6caa7f11e11ce44864df3
Thomas Dziedzic <gostrc@gmail.com>
2011-04-02 20:46:50 -0500

    comment out state printing

--- quarters/builder/statuspoller.py
+++ quarters/builder/statuspoller.py
@@ -15,8 +15,8 @@ class StatusPoller( threading.Thread ):
         while 1:
             remote_state = master_state( self.config )
 
-            print( 'master status:', remote_state.get_state_dict() )
-            print( 'local status:', self.local_state.get_state_dict() )
+            #print( 'master status:', remote_state.get_state_dict() )
+            #print( 'local status:', self.local_state.get_state_dict() )
             
             remote_ujids = set( remote_state.get_ujids() ) # master
             local_ujids = set( self.local_state.get_ujids() ) # builder
